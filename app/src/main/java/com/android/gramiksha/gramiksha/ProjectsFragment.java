7e94f0f74bdbc2a309e3f2b5635439142bf0fc46
arpitkansal <16.arpitkansal@gmail.com>
2018-07-08 18:01:23 +0530

    Webview embedded in Projects Fragment

--- app/src/main/java/com/android/gramiksha/gramiksha/ProjectsFragment.java
+++ app/src/main/java/com/android/gramiksha/gramiksha/ProjectsFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toolbar;
 
 
 /**
@@ -26,6 +30,9 @@ public class ProjectsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    WebView webView;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -58,13 +65,21 @@ public class ProjectsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_projects, container, false);
+        View view = inflater.inflate(R.layout.fragment_projects, container, false);
+        webView = (WebView)view.findViewById(R.id.webview_projects);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://gramiksha.in/projects");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+                return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
